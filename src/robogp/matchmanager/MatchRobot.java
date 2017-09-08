57f312afc0414ae640347eb1e32359e4364f4c3d
st148705 <valentino.dicianni@edu.unito.it>
2017-09-08 16:03:41 +0200

    fixes

--- src/robogp/matchmanager/MatchRobot.java
+++ src/robogp/matchmanager/MatchRobot.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class MatchRobot extends RobotMarker {
     private int hitPoints;
     private int lifePoints;
-    private boolean shutDown;
+    private boolean isShutDown;
 
     private Registry[] registries;
 
@@ -32,7 +32,7 @@ public class MatchRobot extends RobotMarker {
     }
 
     public boolean isShutDown() {
-        return shutDown;
+        return isShutDown;
     }
 
     public ArrayList<Integer> getLocked(){
